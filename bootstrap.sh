5cafacbba3eb1b0e4fef2f550b2f09982d01f0f7
anderson <primataxdeus@gmail.com>
2017-12-23 22:33:12 -0200

    Setting up new configs

--- bootstrap.sh
+++ bootstrap.sh
@@ -20,15 +20,6 @@ apt-get -y update >/dev/null 2>&1
 install 'development tools' build-essential
 
 install Git git
-install SQLite sqlite3 libsqlite3-dev
-install memcached memcached
-install Redis redis-server
-install RabbitMQ rabbitmq-server
-
-install PostgreSQL postgresql postgresql-contrib libpq-dev
-sudo -u postgres createuser --superuser ubuntu
-sudo -u postgres createdb -O ubuntu activerecord_unittest
-sudo -u postgres createdb -O ubuntu activerecord_unittest2
 
 # Needed for docs generation.
 update-locale LANG=en_US.UTF-8 LANGUAGE=en_US.UTF-8 LC_ALL=en_US.UTF-8
